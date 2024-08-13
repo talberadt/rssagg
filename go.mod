678d0f2ceb0db47fe3658f9fa7db9eb6504e326f
tberadt <tberadt@cisco.com>
2024-08-13 11:46:38 +0300

    boilerplate for http server complete

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/talberadt/rssagg
+
+go 1.22.4
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	chi "github.com/go-chi/chi/v5"
+	cors "github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port undefined in environment.")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v..", portString)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
